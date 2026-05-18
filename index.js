838a3755f0ab56d0799405b3bbf604f2c5c142d0
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-05-18 10:22:01 +0600

    Getting data from database successfulll

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
+const dotsenv = require('dotenv')
+dotsenv.config()
+const port = process.env.PORT
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URL
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+   const db = client.db('doctorappoinmentspage')
+   const Allappoinment = db.collection('allappoinments')
+
+   app.get('/allappoinmets' , async (req , res) => {
+    const result = await Allappoinment.find().toArray();
+    res.send(result)
+   })
+
+   
+
+
+
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
