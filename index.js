53e878ceeda9e70f9d7fa0f0c0159c928aee9b75
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-05-19 22:03:32 +0600

    Updated api done

--- index.js
+++ index.js
@@ -26,7 +26,33 @@ async function run() {
    const db = client.db('doctorappoinmentspage')
    const Allappoinment = db.collection('allappoinments')
    const AllBooking = db.collection('allbooking')
+  
 
+   app.patch('/allbookings/:id' , async (req , res ) => {
+    const {id} = req.params 
+    const NewData = req.body 
+    const result = await AllBooking.updateOne(
+      { _id : new ObjectId(id)},
+    {$set : NewData })
+    console.log(result)
+    res.send(result)
+   })
+
+   app.delete('/allbookings/:id' , async (req , res ) => {
+
+    const {id} = req.params 
+    const result = await AllBooking.deleteOne({ _id : new ObjectId(id)});
+    console.log(result)
+
+    res.send(result)
+
+   })
+  
+
+   app.get('/allbookings' , async (req , res ) => {
+    const result = await AllBooking.find().toArray()
+    res.send(result)
+   })
 
    app.post('/allbookings' , async (req , res ) => {
     const Data = req.body
