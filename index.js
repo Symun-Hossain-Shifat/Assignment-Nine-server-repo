3f3bf06907ac3ee2239a0c17c4d563f2ba2ad2d7
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-05-21 18:16:52 +0600

    final backend fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const jose = require('jose-cjs')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jose = require('jose-cjs');
+const dotenv = require('dotenv');
 
-const dotsenv = require('dotenv')
-dotsenv.config()
-const port = process.env.PORT || 5000
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = process.env.MONGO_URL
+
+const uri = process.env.MONGO_URL;
 
 
 app.use(cors({
@@ -20,7 +22,15 @@ app.use(cors({
 }));
 
 
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,130 +42,111 @@ const client = new MongoClient(uri, {
 
 
 const jwks = jose.createRemoteJWKSet(
-  new URL (`${process.env.CLIENT_URL}/api/auth/jwks`)
-)
-const Valudateapi = async (req , res , next ) => {
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+);
 
+const Valudateapi = async (req, res, next) => {
+  const authheader = req.headers.authorization;
 
+  if (!authheader) {
+    return res.status(401).json({ message: 'unauthorized' });
+  }
 
+  const token = authheader.split(' ')[1];
 
-const authheader  = req?.headers?.authorization ;
-console.log(authheader)
-if(!authheader){
-  return res.status(401).json({messaage : 'unauthorized'})
-  
-}
-const token = authheader.split(' ')[1];
-console.log(token)
-if(!token){
-   return res.status(401).json({messaage : 'unauthorized'})
-}
-
-try{
- const {payload} =  await jose.jwtVerify(token , jwks)
-console.log(payload)
-next()
-}catch(error){
-  console.log(error)
-return res.status(403).json({ messaage : error.messaage })
-}
-
-
-}
-
+  try {
+    const { payload } = await jose.jwtVerify(token, jwks);
+    req.user = payload;
+    next();
+  } catch (error) {
+    console.log("JWT ERROR:", error);
+    return res.status(403).json({ message: error.message });
+  }
+};
 
 
 async function run() {
   try {
+    await client.connect();
 
+    const db = client.db('doctorappoinmentspage');
+    const Allappoinment = db.collection('allappoinments');
+    const AllBooking = db.collection('allbooking');
 
-      
-   const db = client.db('doctorappoinmentspage')
-   const Allappoinment = db.collection('allappoinments')
-   const AllBooking = db.collection('allbooking')
+   
+    app.get('/allbookings', Valudateapi, async (req, res) => {
+      const email = req.query.email;
+
+      let query = {};
+      if (email) query = { email };
+
+      const result = await AllBooking.find(query).toArray();
+      res.send(result);
+    });
+
+   
+    app.post('/allbookings', Valudateapi, async (req, res) => {
+      const result = await AllBooking.insertOne(req.body);
+      res.send(result);
+    });
+
+   
+    app.patch('/allbookings/:id', Valudateapi, async (req, res) => {
+      const { id } = req.params;
+
+      const result = await AllBooking.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: req.body }
+      );
+
+      res.send(result);
+    });
+
+   
+    app.delete('/allbookings/:id', Valudateapi, async (req, res) => {
+      const { id } = req.params;
+
+      const result = await AllBooking.deleteOne({
+        _id: new ObjectId(id)
+      });
+
+      res.send(result);
+    });
+
+    app.get('/featured', async (req, res) => {
+      const result = await Allappoinment.find().limit(3).toArray();
+      res.send(result);
+    });
+
   
+    app.get('/allappoinmets', async (req, res) => {
+      const result = await Allappoinment.find().toArray();
+      res.send(result);
+    });
 
+    app.get('/allappoinmets/:id', Valudateapi, async (req, res) => {
+      const { id } = req.params;
 
-   app.patch('/allbookings/:id' , Valudateapi , async (req , res ) => {
-    const {id} = req.params 
-    const NewData = req.body 
-    const result = await AllBooking.updateOne(
-      { _id : new ObjectId(id)},
-    {$set : NewData })
-    console.log(result)
-    res.send(result)
-   })
+      const result = await Allappoinment.findOne({
+        _id: new ObjectId(id)
+      });
 
-   app.delete('/allbookings/:id' , Valudateapi , async (req , res ) => {
+      res.send(result);
+    });
 
-    const {id} = req.params 
-    const result = await AllBooking.deleteOne({ _id : new ObjectId(id)});
-    console.log(result)
+    console.log("MongoDB connected successfully");
 
-    res.send(result)
-
-   })
-  
-
-   app.get('/allbookings' , Valudateapi ,  async (req , res ) => {
-
-    const email = req.query.email 
-    console.log(email)
-    let query = {} 
-    if(email){
-      query = { email : email}
-    }
-    const result = await AllBooking.find(query).toArray()
-    res.send(result)
-   })
-
-   app.post('/allbookings' , Valudateapi ,  async (req , res ) => {
-    const Data = req.body
-    const result = await AllBooking.insertOne(Data)
-    console.log(result)
-    res.send(result)
-   })
-   
-
-   app.get('/featured' , async (req , res ) => {
-    const result = await Allappoinment.find().limit(3).toArray()
-    res.send(result)
-   })
-
-   app.get('/allappoinmets/:id' , Valudateapi , async (req , res ) => {
-    const {id} = req.params
-    const result = await Allappoinment.findOne({ _id : new ObjectId(id)});
-    // console.log(result)
-    res.send(result)
-    
-   })
-
-
-
-
-   app.get('/allappoinmets' , async (req , res) => {
-    const result = await Allappoinment.find().toArray();
-    res.send(result)
-   })
-
-   
-
-
-
-   
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-   
+  } catch (error) {
+    console.log("DB ERROR:", error);
   }
 }
+
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Server is running');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
